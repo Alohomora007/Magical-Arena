e26b320a892a2b2e7b226fa26c2613f6c9c1d893
Alohomora007 <linkaman5@gmail.com>
2024-06-06 23:22:44 +0530

    3rd commit

--- Main.java
+++ Main.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Player player1 = new Player(50, 5, 10);
-        Player player2 = new Player(100, 10, 5);
+        int health,strength,attack;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Health of Player A:");
+        health=sc.nextInt();
+        System.out.println("Enter Strength of Player A:");
+        strength=sc.nextInt();
+        System.out.println("Enter Attack value of Player A:");
+        attack=sc.nextInt();
+        Player player1 = new Player(health, strength, attack);
+        System.out.println("Enter Health of Player B:");
+        health=sc.nextInt();
+        System.out.println("Enter Strength of Player B:");
+        strength=sc.nextInt();
+        System.out.println("Enter Attack value of Player B:");
+        attack=sc.nextInt();
+        Player player2 = new Player(health, strength, attack);
         Arena arena = new Arena(player1, player2);
         arena.startFight();
     }
