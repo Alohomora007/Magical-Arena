2385ef4ff49b3addcb18c4338aa53276a7d5d029
Alohomora007 <linkaman5@gmail.com>
2024-06-06 17:33:21 +0530

    2st Commit

--- PlayerTest.java
+++ PlayerTest.java
@@ -1,7 +1,5 @@
 import org.junit.jupiter.api.Test;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerTest {
     @Test
